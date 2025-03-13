bc52b0908d06035326ce3567d95db190f02d1dbd
JonatasLemos <jonatasolemos@gmail.com>
2025-03-13 17:32:45 -0300

    feat: add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RegistrationForm from './components/login';
+import LocationType from './components/location_types';
 
 function App() {
   return (
-    <div className="App">
-      <RegistrationForm />
-    </div>
-    
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/location_type">Location Type</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<RegistrationForm />} />
+          <Route path="/location_type" element={<LocationType />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/location_types.js
@@ -0,0 +1,13 @@
+// components/LocationType.js
+import React from 'react';
+
+function LocationType() {
+  return (
+    <div>
+      <h1>Location Type Page</h1>
+      <p>This is the location type page.</p>
+    </div>
+  );
+}
+
+export default LocationType;
